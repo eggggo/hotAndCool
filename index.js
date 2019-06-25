290f3891fa3745a80dd221e4a7112796bec3e4d0
Roooooster <iancool2002@gmail.com>
2019-06-25 09:50:51 -0400

    and dey say

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const Discord = require('discord.js');
 const client  = new Discord.Client();
-const config = require("./config.json")
-
-const token = 'NTkyNzc1MjE1MjY0ODI1MzY3.XREREQ.LTshbeNPOI94GlfxVLoWDQvq6Ao';
-
-
-client.on('ready', () => {
-    console.log(`yes`);
-  });
-
-client.login(config.token);
 
 client.on('message', (message) => {
-  if(message.content == 'Andy hot') {
-    message.channel.sendMessage('Andy also cool');
+  if(message.content == "and dey say") {
+    message.reply("AND DEY SAY AND DEY SAY AND DEY SAY AND DEY SAY");
   }
+
 });
+
+
+client.login('NTkyNzc1MjE1MjY0ODI1MzY3.XREREQ.LTshbeNPOI94GlfxVLoWDQvq6Ao');
+
+
+
+
+
 //ok gene
 //test revision number 2
 //wubalubabdubdub
