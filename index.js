72f7af064735774498535df8337a72d14c7082d6
Roooooster <iancool2002@gmail.com>
2019-06-25 09:11:34 -0400

    New Branch

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const config = require("./config.json")
 
 const token = 'NTkyNzc1MjE1MjY0ODI1MzY3.XREREQ.LTshbeNPOI94GlfxVLoWDQvq6Ao';
 
+
 client.on('ready', () => {
     console.log(`yes`);
   });
 
 client.login(config.token);
 
+client.on('message', (message) => {
+  if(message.content == 'Andy hot') {
+    message.channel.sendMessage('Andy also cool');
+  }
+});
 //ok gene
 //test revision number 2
 //wubalubabdubdub
