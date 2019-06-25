5469c03e29caa2770f5eb2da8556e499721d9b60
eggggo <geneliu2009@gmail.com>
2019-06-24 20:41:44 -0400

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client  = new Discord.Client();
+
+const token = 'NTkyNzc1MjE1MjY0ODI1MzY3.XREREQ.LTshbeNPOI94GlfxVLoWDQvq6Ao';
+
+client.on('ready', () => {
+    console.log(`yes`);
+  });
+
+client.login(token)
