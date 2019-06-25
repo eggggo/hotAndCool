99af3b3048c4b9c21d548b32fd72ba82aa64a9ef
eggggo <geneliu2009@gmail.com>
2019-06-24 21:24:15 -0400

    no token with config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client  = new Discord.Client();
+const config = require("./config.json")
 
 const token = 'NTkyNzc1MjE1MjY0ODI1MzY3.XREREQ.LTshbeNPOI94GlfxVLoWDQvq6Ao';
 
@@ -7,7 +8,7 @@ client.on('ready', () => {
     console.log(`yes`);
   });
 
-client.login(token)
+client.login(config.token);
 
 //ok ian
 //test revision number 2
