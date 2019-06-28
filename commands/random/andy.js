8b4a8571945441d1f91dd444f857398d0ecd16e0
eggggo <52181959+eggggo@users.noreply.github.com>
2019-06-28 14:34:33 -0400

    Update andy.js

--- commands/random/andy.js
+++ commands/random/andy.js
@@ -16,7 +16,7 @@ class andy extends commando.Command {
         message.channel.send(andyJAM);
         message.channel.send('Andy Hot AND Cool'+"\n"
         +"Bot made by pepegga#0982, thesciencewizard#2620, bidoof#5988, and Fireshot90#8033"+"\n"
-        +"This is a random bot that does random things, some of which include playing random Youtube clips(from a set list) at random audio qualities(definitely programmed and not due to ytdl-core), replying to certain responses, and rolling random responses, all a result of our random server.");
+        +"This is a random bot that does random things, some of which include playing random Youtube clips(from a set list), replying to certain responses, and rolling random responses, all a result of our random server.");
     }
 }
 
