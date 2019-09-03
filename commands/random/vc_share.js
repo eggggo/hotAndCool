8f815280e2928ab02c10c6e831550b9225432330
eggggo <52181959+eggggo@users.noreply.github.com>
2019-09-03 17:48:33 -0400

    Create vc_share.js

---
+++ commands/random/vc_share.js
@@ -0,0 +1,28 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js')
+
+class vcshare extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "vcshare",
+            group: "random",
+            memberName: "vcshare",
+            description: "gives a link for users in a vc to be able to share screens and camera views"
+        });
+    }
+
+    async run(message, args) {
+        var baseLink = "http://www.discordapp.com/channels/"
+        var serverID = message.guild.id
+        var vcID = message.member.voiceChannelID
+        if(serverID && vcID){
+            message.channel.send(baseLink+serverID+"/"+vcID)
+        }
+        else{
+            const conf = new Discord.Attachment('https://cdn.discordapp.com/attachments/592779094769401924/593098453869920257/confused.jpg')
+            message.channel.send(conf);
+        }
+    }
+}
+
+module.exports = vcshare;
