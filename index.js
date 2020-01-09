4a6e4b1691165f895d3622610f7845166ba642f8
eggggo <52181959+eggggo@users.noreply.github.com>
2020-01-08 21:56:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -169,6 +169,7 @@ client.on('message', message=>{
 })
 
 client.on('messageUpdate', (oldMessage, newMessage) => {
+   if (newMessage.author.id === client.user.id) return
    message.channel.send('buh');
    if(newMessage.content.includes('<:geneHead:627501925033705482>')){
      const andyHead = client.emojis.find(emoji => emoji.name === "andyHead");
