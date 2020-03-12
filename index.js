3e2cf078413eb70a3ed2f3e6435b5eb7f4a4f1fe
eggggo <52181959+eggggo@users.noreply.github.com>
2020-03-12 11:02:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -179,4 +179,15 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
    }
 })
 
+client.on('messageReactionAdd', (messageReaction, user) => {
+if(user.bot)  return;
+const { message, emoji } = messageReaction;
+
+if(emoji.name === "geneHead") {
+    const andyHead = client.emojis.find(emoji => emoji.name === "andyHead");
+     const clap = client.emojis.find(emoji => emoji.name === "clapGif");
+     newMessage.channel.send(`${andyHead} ${clap}`);
+ } 
+});
+
 client.login(process.env.token);
