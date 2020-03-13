7fb4e876258ed8931bd19a4f708b984f3672bf69
eggggo <52181959+eggggo@users.noreply.github.com>
2020-03-13 08:41:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -179,10 +179,6 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
    }
 })
 
-client.on('messageReactionRemove', (reaction, user) => {
-    console.log('a reaction has been removed');
-})
-
 client.on('messageReactionAdd', (messageReaction, user) => {
 if(user.bot)  return;
 const { message, emoji } = messageReaction;
@@ -192,31 +188,5 @@ if(emoji.name === "geneHead") {
      newMessage.channel.send(`${andyHead} ${clap}`);
  } 
 })
-
-client.on('raw', packet => {
-    // We don't want this to run on unrelated packets
-    if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
-    // Grab the channel to check the message from
-    const channel = client.channels.get(packet.d.channel_id);
-    // There's no need to emit if the message is cached, because the event will fire anyway for that
-    if (channel.messages.has(packet.d.message_id)) return;
-    // Since we have confirmed the message is not cached, let's fetch it
-    channel.fetchMessage(packet.d.message_id).then(message => {
-        // Emojis can have identifiers of name:id format, so we have to account for that case as well
-        const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-        // This gives us the reaction we need to emit the event properly, in top of the message object
-        const reaction = message.reactions.get(emoji);
-        // Adds the currently reacting user to the reaction's users collection.
-        if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-        // Check which type of event it is before emitting
-        if (packet.t === 'MESSAGE_REACTION_ADD') {
-            client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
-        }
-        if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-            client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
-        }
-    })
-})
-
 
 client.login(process.env.token);
