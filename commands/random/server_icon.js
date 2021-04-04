8a2bbf4eb4cae09260c54d1b81eb6632c8abf1f1
eggggo <geneliu2009@gmail.com>
2021-04-04 16:07:54 -0400

    server_icon cmd

--- commands/random/server_icon.js
+++ commands/random/server_icon.js
@@ -1,12 +1,13 @@
 const commando = require('discord.js-commando');
+const Discord = require('discord.js')
 const fs = require('fs');
 
 class ServerIconCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: "serverIcon",
+            name: "server_icon",
             group: "random",
-            memberName: "serverIcon",
+            memberName: "server_icon",
             description: "mix it up"
         });
     }
@@ -14,30 +15,32 @@ class ServerIconCommand extends commando.Command {
     async run(message, args) {
         if (!args) {
             message.channel.send('plox add an argument')
-            const embed = new Discord.MessageEmbed()
-            .setTitle('Commands list')
-            .setColor('#DAF7A6')
-            .addFields(
-            {name: 'available commands:',
-            value:"`random`\n`andy`\n`bene`\n`david`\n`edward`\n`ethan`\n`gene`\n`ianh`\n`ianx`\n`joe`\n`john`\n`jon`\n`justin`\n`patrick`\n`ryan`\n`memes`"}
-            )
-            message.channel.send(embed);
-        }
-        if (args == 'random') {
-            var topicName = '';
-            fs.readdir('../iconPics', (err, files) => {
+            message.channel.send('available commands:')
+            message.channel.send('random`\n`andy`\n`bene`\n`david`\n`edward`\n`ethan`\n`gene`\n`ianh`\n`ianx`\n`joe`\n`john`\n`jon`\n`justin`\n`patrick`\n`ryan`\n`memes')
+        } else {
+            if (args == 'random') {
+                var files = fs.readdirSync('./iconPics');
                 var numTopics = files.length;
                 var topic = Math.floor(Math.random() * numTopics);
-                topicName = files[topic];
-            });
-            var tgtPic = '';
-            fs.readdir('../iconPics/' + topicName, (err, files) => {
+                var topicName = files[topic];
+                files = fs.readdirSync('./iconPics/'+topicName);
                 var numPics = files.length;
                 var randomPic = Math.floor(Math.random() * numPics);
-                tgtPic = files[randomPic];
-            })
-            const pic = new MessageAttachment('../iconPics/' + topicName + '/' + tgtPic);
-            message.channel.send(pic);
+                var tgtPic = files[randomPic];
+                const pic = './iconPics/' + topicName + '/' + tgtPic;
+                message.guild.setIcon(pic);
+            } else {
+                try {
+                    files = fs.readdirSync('./iconPics/' + args);
+                    var numPics = files.length;
+                    var randomPic = Math.floor(Math.random() * numPics);
+                    var tgtPic = files[randomPic];
+                    const pic = './iconPics/' + args + '/' + tgtPic;
+                    message.guild.setIcon(pic);
+                } catch (error) {
+                    message.channel.send('we have no pics of what you asked for sowwy');
+                }
+            }
         }
     }
 }
