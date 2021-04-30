a9ae4fe8986b20311a92f25246325f86449d3a16
Samuel Levshteyn <57333560+SamuelLev24@users.noreply.github.com>
2021-04-30 16:13:17 -0400

    Update 15.js

--- commands/random/15.js
+++ commands/random/15.js
@@ -1,8 +1,7 @@
 const commando = require('discord.js-commando');
 const Discord = require('discord.js')
-const fs = require('fs');
 
-class ServerIconCommand extends commando.Command {
+class FifteenCommand extends commando.Command {
     constructor(client) {
         super(client, {
             name: "15",
@@ -12,11 +11,11 @@ class ServerIconCommand extends commando.Command {
         });
     }
 
-    async run(message) {
+    async run(message, args) {
         message.channel.send('yikes, you're kinda hardstuck');
         const hardstuckmp3 = new Discord.Attachment('hardstuck.mp3');
         message.channel.send(hardstuckmp3);
        }
 }
 
-
+module.exports = FifteenCommand;
